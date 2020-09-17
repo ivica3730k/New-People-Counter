02f699160d8c514aee584f8d1cefcf5474624c73
IcE <ivica-matic@outlook.com>
2020-09-18 00:20:16 +0100

    Moved all core components to their separate threads

--- src/data.py
+++ src/data.py
@@ -1,126 +1,131 @@
 import datetime
 import pickle
-
-def save_obj(obj, name ):
-    with open(name, 'wb') as f:
-        pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
-
-def load_obj(name ):
-    try:
-        with open(name, 'rb') as f:
-            return pickle.load(f)
-    except:
-        return {}
-
-_data = load_obj("data.data")
-_visits = 0
-_laser_registrations = 0
+import time
 
 
-def write_visits(num_data=0):
-    if num_data == 0:
-        return
-    current_day = datetime.datetime.now()
-    current_day = current_day.replace(hour=0, minute=0, second=0, microsecond=0).date()
-    current_time = datetime.datetime.now()
-    current_time = current_time.replace(minute=0, second=0, microsecond=0).time()
-    try:
-        _data[current_day]
-    except KeyError:
-        _data[current_day] = {}
-        _data[current_day]["visit"] = {}
-        _data[current_day]["total"] = {}
-    try:
-        old = _data[current_day]["visit"][current_time]
-        _data[current_day]["visit"][current_time] = old + num_data
-        total = 0
-        for key, value in _data[current_day]["visit"].items():
-            total += value
-        _data[current_day]["total"] = total
-    except KeyError:
-        _data[current_day]["visit"][current_time] = num_data
-        total = 0
-        for key, value in _data[current_day]["visit"].items():
-            total += value
-        _data[current_day]["total"] = total
+class Data():
+    _data = {}
+    _laser_registrations = int(0)
+    _lock = False
 
-    save_obj(_data, "data.data")
+    def __init__(self):
+        self._data = self.load_obj("data.data")
 
+    def save_obj(self, obj, name):
+        with open(name, 'wb') as f:
+            pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
 
-def export_csv(data):
-    csv = ""
-    days = []
-    for i in data:
-        days.append(i)
-    hours = []
-    for i in days:
-        for j in data[i]["visit"]:
-            hours.append(j)
-    res = []
-    [res.append(x) for x in hours if x not in res]
-    res.sort()
-    hours = res
+    def load_obj(self, name):
+        try:
+            with open(name, 'rb') as f:
+                return pickle.load(f)
+        except:
+            return {}
 
-    csv += "Datum,"
-    for i in hours:
-        csv += str(i)
-        csv += ","
-    csv += "Ukupno"
-    csv += "\n"
+    def write_visits(self, visits=0):
+        if visits == 0:
+            return
+        current_day = datetime.datetime.now()
+        current_day = current_day.replace(hour=0, minute=0, second=0, microsecond=0).date()
+        current_time = datetime.datetime.now()
+        current_time = current_time.replace(minute=0, second=0, microsecond=0).time()
+        try:
+            self._data[current_day]
+        except KeyError:
+            self._data[current_day] = {}
+            self._data[current_day]["visit"] = {}
+            self._data[current_day]["total"] = {}
+        try:
+            old = self._data[current_day]["visit"][current_time]
+            self._data[current_day]["visit"][current_time] = old + visits
+            total = 0
+            for key, value in self._data[current_day]["visit"].items():
+                total += value
+            self._data[current_day]["total"] = total
+        except KeyError:
+            self._data[current_day]["visit"][current_time] = visits
+            total = 0
+            for key, value in self._data[current_day]["visit"].items():
+                total += value
+            self._data[current_day]["total"] = total
 
-    for i in days:
-        csv += str(i) + ","
-        for j in hours:
-            try:
-                csv += str(data[i]["visit"][j])
-            except KeyError:
-                csv += str(0)
+        self.save_obj(self._data, "data.data")
+
+    def export_csv(self, data):
+        csv = ""
+        days = []
+        for i in data:
+            days.append(i)
+        hours = []
+        for i in days:
+            for j in data[i]["visit"]:
+                hours.append(j)
+        res = []
+        [res.append(x) for x in hours if x not in res]
+        res.sort()
+        hours = res
+
+        csv += "Datum,"
+        for i in hours:
+            csv += str(i)
             csv += ","
-        csv += str(data[i]["total"])
+        csv += "Ukupno"
         csv += "\n"
 
-    return csv
+        for i in days:
+            csv += str(i) + ","
+            for j in hours:
+                try:
+                    csv += str(data[i]["visit"][j])
+                except KeyError:
+                    csv += str(0)
+                csv += ","
+            csv += str(data[i]["total"])
+            csv += "\n"
 
-def getAll(start = None, end = None):
-    if start and end:
-        start_date = datetime.datetime.strptime(start, "%Y-%m-%d").date()
-        end_date = datetime.datetime.strptime(end, "%Y-%m-%d").date()
-        res = {}
-        workdata = _data
-        for i in workdata:
-            if start_date <= i <= end_date:
-                res[i] = workdata[i]
-        return res
+        return csv
 
-    elif start:
-        start_date = datetime.datetime.strptime(start, "%Y-%m-%d").date()
-        res = {}
-        workdata = _data
-        for i in workdata:
-            if i >= start_date:
-                res[i] = workdata[i]
-        return res
-    elif end:
-        print("End")
-        end_date = datetime.datetime.strptime(end, "%Y-%m-%d").date()
-        res = {}
-        workdata = _data
-        for i in workdata:
-            if i <= end_date:
-                res[i] = workdata[i]
-        return res
-    else:
-        return _data
-    
-def laserRegistration():
-    global _laser_registrations
-    global _visits
-    _laser_registrations += 1
-    if _laser_registrations % 2 == 0:
-        _visits += 1
+    def getAll(self, start=None, end=None):
+        if start and end:
+            start_date = datetime.datetime.strptime(start, "%Y-%m-%d").date()
+            end_date = datetime.datetime.strptime(end, "%Y-%m-%d").date()
+            res = {}
+            workdata = self._data
+            for i in workdata:
+                if start_date <= i <= end_date:
+                    res[i] = workdata[i]
+            return res
 
-def obtainVisits():
-    global _visits
-    value = _visits
-    _visits = 0
-    return value
+        elif start:
+            start_date = datetime.datetime.strptime(start, "%Y-%m-%d").date()
+            res = {}
+            workdata = self._data
+            for i in workdata:
+                if i >= start_date:
+                    res[i] = workdata[i]
+            return res
+        elif end:
+            end_date = datetime.datetime.strptime(end, "%Y-%m-%d").date()
+            res = {}
+            workdata = self._data
+            for i in workdata:
+                if i <= end_date:
+                    res[i] = workdata[i]
+            return res
+        else:
+            return self._data
+
+    def laserRegistration(self):
+        self._lock = True
+        self._laser_registrations = self._laser_registrations + 1
+        self._lock = False
+
+    def obtainVisits(self):
+        while self._lock:
+            time.sleep(1)
+        value = int(self._laser_registrations / 2)
+        self._laser_registrations = int(self._laser_registrations % 2)
+        return value
+
+
+data = Data()
--- src/main.py
+++ src/main.py
@@ -1,65 +1,75 @@
-import datetime
 import threading
 import time
+
 import config as conf
-import data as data
-from flask import Flask, render_template, request, Response
 import serial
+from data import data
+from flask import Flask, render_template, request, Response
 
 app = Flask(__name__)
 _serial_ok = False
 
 
 def writeVisits():
-    global _times_written
-    print("Started writing thread")
     try:
         while True:
-            data.write_visits(data.obtainVisits())
-            time.sleep(10)
+            visits = data.obtainVisits()
+            data.write_visits(visits)
+            time.sleep(1)
     except KeyboardInterrupt:
         return
 
+
 def processSerialPort():
-    print("Started serial port processing thread")
     global _serial_ok
-    ARDUINO_COM_PORT = conf.ARDUINO_COM_PORT
-    ARDUINO_BAUD_RATE = conf.ARDUINO_BAUD_RATE
-    ser = serial.Serial(ARDUINO_COM_PORT,ARDUINO_BAUD_RATE, timeout=1)
-    _serial_ok = True
-    try:
-        while True:
-            ser.readline()
-            data.laserRegistration()
-    except:
-        _serial_ok = False
+    global _reset_serial
+    while True:
+        try:
+            while True:
+                conf.read_config_file()
+                ARDUINO_COM_PORT = conf.ARDUINO_COM_PORT
+                ARDUINO_BAUD_RATE = conf.ARDUINO_BAUD_RATE
+                ser = serial.Serial(ARDUINO_COM_PORT, ARDUINO_BAUD_RATE)
+                _serial_ok = True
+                while True:
+                    result = (int(ser.readline()))
+                    if result == 1:
+                        data.laserRegistration()
+        except KeyboardInterrupt:
+            return
+        except:
+            time.sleep(1)
+            _serial_ok = False
 
-
-
-writingThread = threading.Thread(target=writeVisits)
-serialPortProcessingThread = threading.Thread(target=processSerialPort)
 
 @app.route('/')
 def table():
     start = request.args.get('start')
     end = request.args.get('end')
-    return render_template('table.html', data=data.getAll(start,end),start = start, end = end)
+    return render_template('table.html', data=data.getAll(start, end), start=start, end=end, ser_ok=_serial_ok)
+
 
 @app.route('/csv')
 def csv():
     start = request.args.get('start')
     end = request.args.get('end')
-    csv = data.export_csv(data.getAll(start,end))
+    csv = data.export_csv(data.getAll(start, end))
     return Response(
         csv,
         mimetype="text/csv",
         headers={"Content-disposition":
-                 "attachment; filename=podatci.csv"})
+                     "attachment; filename=podatci.csv"})
 
-    
 
+def flask():
+    app.run(debug=True, use_reloader=False)
+
+
+writingThread = threading.Thread(target=writeVisits)
+serialPortProcessingThread = threading.Thread(target=processSerialPort)
+webServerThread = threading.Thread(target=flask)
 
 if __name__ == '__main__':
-    writingThread.start()
     serialPortProcessingThread.start()
-    app.run(debug=True)
+    writingThread.start()
+    webServerThread.start()
